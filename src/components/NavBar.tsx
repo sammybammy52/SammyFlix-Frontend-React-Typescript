31de95da47bc53f6f62e24ce60c6a15b2b287a51
Seye Yemi-Olowolabi <72563374+sammybammy52@users.noreply.github.com>
2023-02-12 20:04:33 +0100

    Responsive nav

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,9 +1,13 @@
 import { useState, useEffect } from "react";
-import { PlayCircleIcon } from "@heroicons/react/24/outline";
+import { PlayCircleIcon, Bars3Icon } from "@heroicons/react/24/outline";
 import { Link } from "react-router-dom";
 const NavBar = () => {
   const [fix, setFix] = useState(false);
+  const [mobileToggle, setMobileToggle] = useState(false);
 
+  const handleMobileNav = () => {
+    setMobileToggle(!mobileToggle);
+  }
   function setFixed(): void {
     //console.log(window.scrollY)
     if (
@@ -24,45 +28,71 @@ const NavBar = () => {
     <div
       className={
         fix
-          ? "  w-[100%] pt-3 h-20 fixed z-20 bg-black transition-all duration-300"
-          : "  w-[100%] pt-3 h-20 fixed z-[2] bg-transparent transition-all duration-300"
+          ? "  w-[100%] h-20 fixed z-20 bg-black transition-all duration-300 max-[800px]:bg-black"
+          : "  w-[100%] h-20 fixed z-[2] bg-transparent transition-all duration-300 max-[800px]:bg-black"
       }
-    > 
-      <div className="desktop-nav grid grid-cols-4">
-      <Link to="/">
-        <div className="logoDiv flex justify-center justify-items-center gap-2">
-          <PlayCircleIcon className="h-12 w-12 text-purple-800" />
-          <h3 className="nav-h3 text-white py-3">SAMMYFLIX</h3>
-        </div>
-      </Link>
+    >
+      <div className="desktop-nav grid grid-cols-4 pt-3 max-[800px]:hidden">
+        <Link to="/">
+          <div className="logoDiv flex justify-center justify-items-center gap-2">
+            <PlayCircleIcon className="h-12 w-12 text-purple-800" />
+            <h3 className="nav-h3 text-white py-3">SAMMYFLIX</h3>
+          </div>
+        </Link>
 
-      <div className="linksDiv col-span-2 py-3">
-        <ul className="flex text-white justify-center gap-14">
-          <li>
-            {" "}
-            <a href="#">Home</a>{" "}
-          </li>
-          <li>
-            {" "}
-            <a href="#">Movies</a>{" "}
-          </li>
-          <li>
-            {" "}
-            <a href="#">Series</a>{" "}
-          </li>
-          <li>
-            {" "}
-            <a href="#">About</a>{" "}
-          </li>
-        </ul>
+        <div className="linksDiv col-span-2 py-3">
+          <ul className="flex text-white justify-center gap-14">
+            <li>
+              {" "}
+              <a href="#">Home</a>{" "}
+            </li>
+            <li>
+              {" "}
+              <a href="#">Movies</a>{" "}
+            </li>
+            <li>
+              {" "}
+              <a href="#">Series</a>{" "}
+            </li>
+            <li>
+              {" "}
+              <a href="#">About</a>{" "}
+            </li>
+          </ul>
+        </div>
+        <div className="loginBtn flex justify-center pb-4">
+          <button className="login-btn text-white p-3 bg-purple-800  w-[100px]">
+            Login
+          </button>
+        </div>
       </div>
-      <div className="loginBtn flex justify-center pb-4">
-        <button className="login-btn text-white p-3 bg-purple-800  w-[100px]">
-          Login
-        </button>
+
+      <div className="mobile-nav pt-3 hidden max-[800px]:block">
+        <div className="flex justify-between px-3">
+          <Link to="/">
+          <div className="logoDiv flex justify-items-center gap-2">
+            <PlayCircleIcon className="h-12 w-12 text-purple-800" />
+            <h3 className="nav-h3 text-white py-3">SAMMYFLIX</h3>
+          </div>
+        </Link>
+
+        <div>
+          <Bars3Icon className="h-12 w-12 text-white drop-shadow-lg hover:scale-110 transition-all" onClick={handleMobileNav}/>
+        </div>
+        </div>
+
+        <div className={ mobileToggle ? "mobile-nav-content bg-black w-full transition duration-500": "h-0 opacity-0 duration-500 transition-all "}>
+          <ul className={mobileToggle ? "text-white p-5 transition-all delay-300" : " delay-300 hidden transition-all"}>
+            <li className="py-5 border-b-[0.3px] border-gray-700 text-center">Home</li>
+            <li className="py-5 border-b-[0.3px] border-gray-700 text-center">Movies</li>
+            <li className="py-5 border-b-[0.3px] border-gray-700 text-center">Series</li>
+            <li className="py-5 border-b-[0.3px] border-gray-700 text-center">About</li>
+          </ul>
+        </div>
+        
+
+
       </div>
-      </div>
-      
     </div>
   );
 };
